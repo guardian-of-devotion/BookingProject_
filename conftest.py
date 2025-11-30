fd85df5c6b9646a5c1e82ec67053d7d98eba809c
KittenAttack <guardian-of-devotion@yandex.ru>
2025-11-30 21:11:54 +0300

    Написал фикстуры api_client, booking_dates и generate_random_booking_data

---
+++ conftest.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from core.clients.api_client import APIClient
+import pytest
+from faker import Faker
+
+@pytest.fixture(scope="session")
+def api_client():
+    client = APIClient()
+    client.auth()
+    return client
+
+
+@pytest.fixture
+def booking_dates():
+    today = datetime.today()
+    checkin_date = today + timedelta(days=10)
+    checkout_date = today + timedelta(days=5)
+
+    return {
+        "checkin": checkin_date.strftime("%Y-%m-%d"),
+        "checkout": checkout_date.strftime("%Y-%m-%d"),
+    }
+
+@pytest.fixture
+def generate_random_booking_data(booking_dates):
+    faker = Faker()
+    firstname = faker.first_name()
+    lastname = faker.last_name()
+    totalprice = faker.random_number(digits=3)
+    depositpaid = faker.boolean()
+    additionalneeds = faker.sentence()
+
+    data = {
+        "first_name": firstname,
+        "last_name": lastname,
+        "totalprice": totalprice,
+        "depositpaid": depositpaid,
+        "additionalneeds": additionalneeds,
+    }
+
+    return data
