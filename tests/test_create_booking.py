cc7eb23dc20528b58cf08a46e45d4b6260af6d52
KittenAttack <guardian-of-devotion@yandex.ru>
2025-12-04 21:07:18 +0300

    Вынес все тесты в один файл

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -1,9 +1,12 @@
 import allure
+import pytest
 from pydantic import ValidationError
 from core.models.booking import BookingResponse
+from requests.exceptions import HTTPError
 
 @allure.feature('Booking creation')
 @allure.title('Test create booking')
+
 
 def test_create_booking(api_client):
     with allure.step('Create booking'):
@@ -29,3 +32,60 @@ def test_create_booking(api_client):
 
         assert actual.items() >= expected.items()
         assert actual["bookingdates"].items() >= expected["bookingdates"].items()
+
+
+@allure.title('Test create booking with only required fields')
+def test_create_booking_with_only_required_fields(api_client):
+    with allure.step('Create booking'):
+        booking_data = {
+            "firstname": "Jim",
+            "lastname": "Brown",
+            "totalprice": 111,
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": "2018-01-01",
+                "checkout": "2019-01-01"
+            },
+        }
+        response = api_client.create_booking(booking_data)
+        try:
+            BookingResponse(**response)
+        except ValidationError as e:
+            raise ValidationError(f"Response validation failed: {e}")
+
+        expected = booking_data
+        actual = response["booking"]
+
+        assert actual.items() >= expected.items()
+        assert actual["bookingdates"].items() >= expected["bookingdates"].items()
+
+
+@allure.title('Test create booking without required fields')
+def test_create_booking_without_required_fields(api_client):
+    with allure.step('Create booking'):
+        booking_data = {
+            "additionalneeds": "blablabla"
+        }
+        with pytest.raises(HTTPError) as e:
+            api_client.create_booking(booking_data)
+
+        with allure.step('Assert code 500'):
+            assert e.value.response.status_code == 500
+
+
+@allure.title('Test create booking with random data')
+def test_create_booking_random_data(api_client, generate_random_booking_data):
+    with allure.step("Create booking with random data"):
+        booking_data = generate_random_booking_data
+        response = api_client.create_booking(booking_data)
+        try:
+            BookingResponse(**response)
+        except ValidationError as e:
+            raise ValidationError(f"Response validation failed: {e}")
+
+        actual = response["booking"]
+
+        assert actual.items() >= booking_data.items()
+        assert actual["bookingdates"].items() >= booking_data["bookingdates"].items()
+
+
